0cfcd9d8c999ec694fc94b2de0ef5ebb18e6534b
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-07-15 15:20:27 +0900

    commit

---
+++ src/main/java/com/korea/dto/PointshopDTO.java
@@ -0,0 +1,12 @@
+package com.korea.dto;
+import lombok.Data;
+
+@Data
+public class PointshopDTO {
+	int no;
+	String name;
+	String discription;
+	int price;
+	String staticImage;
+	String gifImage;
+}
