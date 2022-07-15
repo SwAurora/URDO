7d1531bb6f302d5160e7fde979c7b6908777489b
Jay <qjfwlq475@naver.com>
2022-07-15 13:24:58 +0900

    -commit test (KimJaeI)

--- src/main/java/com/korea/dto/MemberDTO.java
+++ src/main/java/com/korea/dto/MemberDTO.java
@@ -2,6 +2,10 @@ package com.korea.dto;
 
 import lombok.Data;
 
+/**
+ * @author Administrator
+ *
+ */
 @Data
 public class MemberDTO
 {
@@ -11,4 +15,48 @@ public class MemberDTO
     private String nickname;
     private int memberpoint;
     private int grade;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getMemberpoint() {
+		return memberpoint;
+	}
+	public void setMemberpoint(int memberpoint) {
+		this.memberpoint = memberpoint;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	@Override
+	public String toString() {
+		return "MemberDTO [id=" + id + ", pw=" + pw + ", email=" + email + ", nickname=" + nickname + ", memberpoint="
+				+ memberpoint + ", grade=" + grade + "]";
+	}
+    
+
 }
