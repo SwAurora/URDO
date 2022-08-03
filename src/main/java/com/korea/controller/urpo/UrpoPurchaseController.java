d895c6018b5f2789581f31d90963c28a43783c18
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-08-03 21:30:58 +0900

    로그인x - 포인트샵 구매 막기

--- src/main/java/com/korea/controller/urpo/UrpoPurchaseController.java
+++ src/main/java/com/korea/controller/urpo/UrpoPurchaseController.java
@@ -27,17 +27,7 @@ public class UrpoPurchaseController implements SubController{
 		int price = Integer.parseInt(req.getParameter("price"));
 		req.setAttribute("dto", dto);
 		
-		// 해당 아이디 회원 정보를 dto 형태로 불러왔음. UrpoDTO와 구분하기 위해 mdto사용
-		mdto = mservice.MemberSearch(id);
-		
-		// 아이템 구매여부 확인 플래그 exist
-		String[] nolist = mdto.getItems().split(";");
-		boolean exist = false;
-		for(int i=0; i<nolist.length; i++) {
-			if(nolist[i].contains(no + "")) {
-				exist = true;
-			} 
-		}
+
 		
 		
 		// 로그인, 포인트 잔액, 구매여부 검증
@@ -48,29 +38,44 @@ public class UrpoPurchaseController implements SubController{
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(mdto.getPoint() < price) {
-			System.out.println("잔액 : " + mdto.getPoint());
-			req.setAttribute("msg", "포인트가 부족합니다. 남은 포인트 : " +mdto.getPoint());
-			try {
-				req.getRequestDispatcher("/URPO/urconRead.jsp").forward(req, resp);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else if(exist) {
-			req.setAttribute("msg", "동일한 아이템을 이미 구매하셨습니다.");
-			try {
-				req.getRequestDispatcher("/URPO/urconRead.jsp").forward(req, resp);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		} else {
-			service.purchase(mdto, price, no);
-			int remains = mdto.getPoint() - price;
-			req.setAttribute("msg", "구매 성공! 남은 포인트 : " + remains);
-			try {
-				req.getRequestDispatcher("/URPO/urconRead.jsp").forward(req, resp);
-			} catch (Exception e) {
-				e.printStackTrace();
+		
+			// 해당 아이디 회원 정보를 dto 형태로 불러왔음. UrpoDTO와 구분하기 위해 mdto사용
+			mdto = mservice.MemberSearch(id);
+			
+			// 아이템 구매여부 확인 플래그 exist
+			String[] nolist = mdto.getItems().split(";");
+			boolean exist = false;
+			for(int i=0; i<nolist.length; i++) {
+				if(nolist[i].contains(no + "")) {
+					exist = true;
+				} 
+			}
+		
+			if(mdto.getPoint() < price) {
+				System.out.println("잔액 : " + mdto.getPoint());
+				req.setAttribute("msg", "포인트가 부족합니다. 남은 포인트 : " +mdto.getPoint());
+				try {
+					req.getRequestDispatcher("/URPO/urconRead.jsp").forward(req, resp);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if(exist) {
+				req.setAttribute("msg", "동일한 아이템을 이미 구매하셨습니다.");
+				try {
+					req.getRequestDispatcher("/URPO/urconRead.jsp").forward(req, resp);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				service.purchase(mdto, price, no);
+				int remains = mdto.getPoint() - price;
+				req.setAttribute("msg", "구매 성공! 남은 포인트 : " + remains);
+				try {
+					req.getRequestDispatcher("/URPO/urconRead.jsp").forward(req, resp);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		
