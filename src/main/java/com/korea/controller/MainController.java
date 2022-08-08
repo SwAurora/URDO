29eb2b317876b05c87f7596d614deceb17550d27
rlatj <rlatjddnr74@gmail.com>
2022-08-08 23:15:20 +0900

    2022-08-08 master(ver11)

--- src/main/java/com/korea/controller/MainController.java
+++ src/main/java/com/korea/controller/MainController.java
@@ -3,6 +3,7 @@ package com.korea.controller;
 import com.korea.dto.BoardDTO;
 import com.korea.service.BoardService;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 public class MainController implements SubController
 {
     BoardService service = BoardService.getInstance();
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse resp)
     {
@@ -21,6 +23,7 @@ public class MainController implements SubController
             req.setAttribute("list", list);
             req.setAttribute("list2", list2);
             req.setAttribute("list3", list3);
+
             req.getRequestDispatcher("main.jsp").forward(req, resp);
         }
         catch(Exception e)
